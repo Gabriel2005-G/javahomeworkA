2d948b039a69af697e99c5e5602c7ed239d7e761
Gabriel2005-G <gghukasyan783@gmail.com>
2026-01-09 19:39:14 +0400

    Add files via upload

---
+++ course.java
@@ -0,0 +1,69 @@
+// int age = 25;
+
+// if (age >= 18 && age <= 65) {
+//     System.out.println("Allowed");
+// } else {
+//     System.out.println("Not allowed");
+// }
+
+// boolean hasTicket = false;
+// boolean hasInvitation = true;
+
+// if (hasTicket || hasInvitation) {
+//     System.out.println("Allowed");
+// } else {
+//     System.out.println("Not allowed");
+// }
+
+
+// int a = 5;
+// int b = -3;
+// int c = 10;
+
+// if ((a > 0 && b > 0) ||
+//     (a > 0 && c > 0) ||
+//     (b > 0 && c > 0)) {
+//     System.out.println("At least two are positive");
+// } else {
+//     System.out.println("Less than two are positive");
+// }
+
+// int x = 5;   
+// int y = 3;   
+
+// int result = x | y;
+
+// System.out.println("x = " + x);
+// System.out.println("y = " + y);
+// System.out.println("result = " + result);
+
+// int a = 7;
+// int b = 7;
+
+// if (a > b) {
+//     System.out.println(a);
+// } else if (b > a) {
+//     System.out.println(b);
+// } else {
+//     System.out.println("Equal");
+// }
+
+// int score = 85;
+
+// if (score >= 90 && score <= 100) {
+//     System.out.println("Excellent");
+// } else if (score >= 70) {
+//     System.out.println("Good");
+// } else if (score >= 50) {
+//     System.out.println("Average");
+// } else {
+//     System.out.println("Fail");
+// }
+
+// int n = 7;
+
+// if ((n & 1) == 0) {
+//     System.out.println("Even");
+// } else {
+//     System.out.println("Odd");
+// }
